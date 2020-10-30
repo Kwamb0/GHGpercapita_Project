8a37616892fc1e1ba39be3b0d2e3703d116c7627
Kanishka Ramanan <kanishka.rama1997@gmail.com>
2020-10-29 20:20:42 -0700

    feat. Created Polynomial Regression ML Model

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 # Create API of ML model using flask
 
-'''
-This code takes the JSON data while POST request an performs the prediction using loaded model and returns
-the results in JSON format.
-'''
-
 # Import libraries
 import numpy as np
 from flask import Flask, render_template, request, jsonify
 import pickle
+from sklearn.preprocessing import PolynomialFeatures
 
 app = Flask(__name__)
 
 # Load the model
-model = pickle.load(open('./ML_Script/ghg_linear_model.pkl','rb'))
+lin_model = pickle.load(open('./ML_Script/ghg_linear_model.pkl','rb'))
+poly_model = pickle.load(open('./ML_Script/ghg_poly_model.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -24,17 +21,19 @@ def home():
 def predict():
     # Get the data from the POST request.
     if request.method == "POST":
-        #data = request.get_json(force=True)
+       
         print(request.form['exp'])
         data = float(request.form['exp'])
-        print("Data", model.predict([[data]]))
+        print("Data", lin_model.predict([[data]]))
+        
         # Make prediction using model loaded from disk as per the data.
-        prediction = model.predict([[data]])
-
+        lin_prediction = lin_model.predict([[data]])
         # Take the first value of prediction
-        output = prediction[0]
+        lin_output = lin_prediction[0]
 
-        return render_template("results.html", output=output, exp=data)
+
+
+        return render_template("results.html", output=lin_output, exp=data)
 
 @app.route('/visualizations/<html_name>')
 def fetch_visual(html_name):
